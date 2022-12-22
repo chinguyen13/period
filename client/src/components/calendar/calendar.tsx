488325be03d2138026280b0d418d22e05b6e3c19
Chi Nguyen <nguyennc1303@gmail.com>
2022-12-23 01:54:04 +0700

    second

--- client/src/components/calendar/calendar.tsx
+++ client/src/components/calendar/calendar.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { Button, Calendar, Popconfirm } from "antd";
+import { Button, Calendar, Popconfirm, Spin } from "antd";
 import type { Dayjs } from 'dayjs';
 import "./calendar.css";
 import dayjs from 'dayjs';
-import Strawberry from "../../images/strawberry.png";
 
 interface Data{
   id: number;
@@ -14,35 +13,37 @@ interface Props{
   data: Data[];
   addPeriod: any;
   currentDate: Dayjs;
+  isLoading: boolean;
+  setIsLoading: any;
 }
+let cycleStart: Date;
 
 const currentDate = dayjs(new Date());
 
 const CalendarComponent: React.FC<Props> = (props: Props) => {
   const [curValue, setCurValue] = React.useState(props.currentDate);
-  
-
   const getListData = (value: Dayjs) => {
-    let listData;
     props.data.forEach(item => {  
-      if(value.isSame(dayjs(item.start_date)))
+      if(value.isSame(dayjs(item.start_date), 'dates'))
       {
-        listData = item;
+        cycleStart = new Date(value.toString());
         return;
       }
     });
-    return listData || undefined;
+    return;
   }
 
   const customCell = (value: Dayjs) => {
-    const listData = getListData(value);
+    getListData(value);
     return(
       <div
         className={
           "customCell "
-          + (listData ? "period " : '')
-          + (value.day() % 6 === 0 ? "weekendCell " : "" )
-          + (value.isSame(currentDate) || value.isAfter(currentDate) ? "futureCell" : "") 
+          + (cycleStart && value.isSame(cycleStart, 'dates') ? "period " : '')
+          // + (value.day() % 6 === 0 ? "weekendCell " : "" )
+          + (value.isSame(currentDate) || value.isAfter(currentDate) ? "futureCell " : "")
+          + (cycleStart && value.isAfter(cycleStart, 'dates') && value.isBefore(dayjs(new Date().setDate(cycleStart.getDate() + 6)), 'dates') ? "menstruation " : "")
+          + (cycleStart && value.isAfter(dayjs(new Date().setDate(cycleStart.getDate() + 10)), 'dates') && value.isBefore(dayjs(new Date().setDate(cycleStart.getDate() + 16)), 'dates') ? "ovulation" : "")
         }
         style={{ backgroundColor: value.isSame(curValue) ? '#E6F4FF' : '', borderTop: value.isSame(currentDate) ? "2px solid #1677ff" : "2px solid #DDD" }}>
         <p style={{textAlign: "right", padding: "10px 10px", color: value.isSame(curValue) ?  "#1677FF" : value.month() !== curValue.month() ? "#00000040" : "#000000"}}>{value.date()}</p>
@@ -50,9 +51,14 @@ const CalendarComponent: React.FC<Props> = (props: Props) => {
     )
   }
 
+  const confirmation = () => {
+    props.addPeriod(curValue);
+    return;
+  }
+
   return(
     <div style={{padding: '10px 20px'}}>
-      
+      {props.isLoading ? <Spin/> : null}
       <Calendar 
         validRange={[dayjs("2022-11-27"), dayjs("2023-12-31")]}
         value={curValue}
@@ -65,7 +71,7 @@ const CalendarComponent: React.FC<Props> = (props: Props) => {
         <Popconfirm
         placement="topRight"
         title={"Are you sure to add period?"}
-        onConfirm={props.addPeriod(curValue)}
+        onConfirm={confirmation}
         okText="Yes"
         cancelText="No"
       >
--- client/src/components/calendar/page.tsx
+++ client/src/components/calendar/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Axios } from "../..";
 import CalendarComponent from "./calendar";
 import dayjs from 'dayjs';
+
 interface Data{
   id: number;
   start_date: string;
@@ -11,21 +12,28 @@ const currentDate = dayjs(new Date());
 
 const CalendarPage: React.FC = () => {
   const [data, setData] = React.useState<Data[]>([]);
-
+  const [isLoading, setIsLoading] = React.useState(false);
+  
   React.useEffect(() => {
+    setIsLoading(true);
     Axios.get('/period').then(res => {
       setData(res.data);
+      setIsLoading(false);
     })
-  },[])
+  }, []);
 
   const addPeriodAction = (value : any) => {
+    setIsLoading(true);
     Axios.post('/period', { "start_date": value }).then(() => {
-
+      Axios.get('/period').then(res => {
+        setData(res.data);
+        setIsLoading(false);
+      })
     })
   }
 
   return(
-    <CalendarComponent data={data} currentDate={currentDate} addPeriod={addPeriodAction}/>
+    <CalendarComponent data={data} setIsLoading={setIsLoading} isLoading={isLoading} currentDate={currentDate} addPeriod={addPeriodAction}/>
   );
 }
 
