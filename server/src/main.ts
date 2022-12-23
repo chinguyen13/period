2888d8ede3e065075a9a3805d99ded6f030c2fcf
GEO\nguyen.chi.nguyen <nguyen.chi.nguyen@geonet.co.jp>
2022-12-23 16:27:03 +0700

    sad story

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-require('dotenv').config();
+// require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(parseInt(process.env.PORT));
+  await app.listen(3006,"0.0.0.0");
 }
 bootstrap();
